62162806b5f82268a6ee8c83b8c2fb4a0941471c
KelyAnamuro <kelyanamurohuamani@gmail.com>
2019-02-22 21:40:41 -0500

    añadiendo productos

--- js/main.js
+++ js/main.js
@@ -5,7 +5,7 @@ let products = [{
     description: "20 packs x 21 gr",
     precioVenta: 95,
     puntoValor: 6.20,
-    img: "img/vitaCafe.png"
+    img: "img/vitacafe.jpg"
 },
 {
     name: "DXN Lingzhi Tea Latte",
@@ -13,7 +13,7 @@ let products = [{
     description: "12 packs x 30 gr",
     precioVenta: 95,
     puntoValor: 7,
-    img: "img/cocozhi.png"
+    img: "img/tealatte.jpg"
 
 },
 {
@@ -22,15 +22,79 @@ let products = [{
     description: "12 packs x 28 gr",
     precioVenta: 95,
     puntoValor:7,
-    img: "img/creamCafe.png"
+    img: "img/whiteCoffeZhino.png"
 },
 {
     name: "DXN Linzhi Coffe 3 en 1",
     price: 50,
     description: "20 packs x 21 gr",
     precioVenta: 75,
+    puntoValor: 4.60,
+    img: "img/3in1.jpg"
+},
+{
+    name: "DXN Spica Tea",
+    price: 50,
+    description: "20 packs x 5 gr",
+    precioVenta: 75,
+    puntoValor: 5.50,
+    img: "img/spica_tea.jpg"
+},
+{
+    name: "DXN Linzhi Black Coffe",
+    price: 50,
+    description: "20 packs x 4.5 gr",
+    precioVenta: 75,
     puntoValor: 4.6,
-    img: "img/cafe3en1.png"
+    img: "img/blackCoffe.png"
+},
+{
+    name: "DXN Cocozhi",
+    price: 60,
+    description: "20 packs x 32 gr",
+    precioVenta: 90,
+    puntoValor: 6,
+    img: "img/cocozhi.jpg"
+},
+{
+    name: "DXN Spirulina Cereal",
+    price: 111,
+    description: "30 packs x 30 gr",
+    precioVenta: 167,
+    puntoValor: 15.6,
+    img: "img/spirulina_cereal.jpg"
+},
+{
+    name: "DXN Zhi Mocha",
+    price: 61,
+    description: "20 packs x 21 gr",
+    precioVenta: 92,
+    puntoValor: 7,
+    img: "img/zhimocha.jpg"
+},
+{
+    name: "DXN Zhi Mint Plus",
+    price: 107,
+    description: "12 packs x 25 gr",
+    precioVenta: 161,
+    puntoValor: 12,
+    img: "img/zhimint.jpg"
+},
+{
+    name: "DXN Zhi Cafe Classic",
+    price: 52,
+    description: "20 packs x 20 gr",
+    precioVenta: 78,
+    puntoValor: 5.50,
+    img: "img/zhicafe.jpg"
+},
+{
+    name: "DXN Cordyceps Cereal",
+    price: 126,
+    description: "20 packs x 30 gr",
+    precioVenta: 189,
+    puntoValor: 19,
+    img: "img/cordy_cereal.jpg"
 }
 ];
 
